185b8a692bbfa5e52fe510361d68b7dbd8fd13eb
Asheya Casey Yuson <asheya.yuson@usjr.edu.ph>
2024-01-24 09:11:13 +0800

    feature: make last name optional in the payment process

--- src/app/pages/start/pay/pay.component.ts
+++ src/app/pages/start/pay/pay.component.ts
@@ -23,7 +23,7 @@ export class PayComponent implements OnInit, OnDestroy {
       Validators.required, Validators.minLength(2), Validators.maxLength(64)]
     ),
     lastName: new FormControl('', [
-      Validators.required, Validators.minLength(2), Validators.maxLength(64)
+      Validators.maxLength(64)
     ]),
     email: new FormControl('', [
       Validators.required, Validators.email, Validators.maxLength(64)
